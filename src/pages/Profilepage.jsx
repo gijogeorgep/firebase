c1b3467a0ab99f2c0098b2df1ffaf84d5224df63
gijo <gijogeorgep02@gmail.com>
2024-12-09 10:25:17 +0530

    updated file name

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { auth, db } from "../Firebase";
+import { auth, db } from "../firebase";
 import { doc } from "firebase/firestore";
 import { getDoc } from "firebase/firestore";
 
